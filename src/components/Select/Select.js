7415354d80a2a6c5a5729a9eda09a51a7fab723a
Kai <human2lkai@gmail.com>
2023-03-08 17:04:17 +1100

    Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { COLORS } from "../../constants";
+import Icon from "../Icon/Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+const Wrapper = styled.div``;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+`;
+
+const CustomSelect = styled.div`
+  position: relative;
+  height: 43px;
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  color: ${COLORS.gray700};
+  padding: 12px 16px;
+  width: max-content;
+  white-space: nowrap;
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <CustomSelect>
+        <NativeSelect value={value} onChange={onChange}>
+          {children}
+        </NativeSelect>
+        {value}
+        <Icon id="chevron-down" size="24" />
+      </CustomSelect>
+    </Wrapper>
   );
 };
 
